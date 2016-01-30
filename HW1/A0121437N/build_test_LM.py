06eecdd2258ad749a6f20c53d222fbc1b73a2ca3
Ian Ngiaw <ianngiaw@Ian-Ngiaws-MacBook-Pro.local>
2016-01-31 00:06:22 +0800

    Added answers to essay questions and a few comments here and there in the code

--- HW1/A0121437N/build_test_LM.py
+++ HW1/A0121437N/build_test_LM.py
@@ -3,103 +3,23 @@ import re
 import nltk
 import sys
 import getopt
+
+# Log function imported from math to be used in probability calculation
 from math import log10
 
-# Code with my model of smoothing
-'''
+# The default ngram size of 4
+NGRAM_SIZE = 4
+
 def build_LM(in_file):
     """
     build language models for each label
     each line in in_file contains a label and an URL separated by a tab(\t)
     """
     print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
-    smoothed_model = {}
-    totals = {}
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        if language not in smoothed_model:
-            smoothed_model[language] = {}
-        if language not in totals:
-            totals[language] = 0
-        char_list = list(split_line[1].strip())
-        for i in range(0, len(char_list) - 3):
-            chars = tuple(char_list[i:i+4])
-            if chars not in smoothed_model[language]:
-                smoothed_model[language][chars] = 2
-                totals[language] += 2
-            else:
-                smoothed_model[language][chars] += 1
-                totals[language] += 1
-    input_file.close()
-    return (smoothed_model, totals)
-    
-def test_LM(in_file, out_file, LM):
-    """
-    test the language models on new URLs
-    each line of in_file contains an URL
-    you should print the most probable label for each URL into out_file
-    """
-    print "testing language models..."
-    # This is an empty method
-    # Pls implement your code in below
-    test_file = file(in_file, 'r')
-    output_file = file(out_file, 'w')
-
-    smoothed_model = LM[0]
-    totals = LM[1]
-
-    for line in test_file:
-        char_list = list(line.strip())
-        results = []
-        for language in smoothed_model:
-            current_total = totals[language]
-            language_model = smoothed_model[language]
-            numerators = []
-            miss_count = 0
-            for i in range(0, len(char_list) - 3):
-                chars = tuple(char_list[i:i+4])
-                if chars in language_model:
-                    numerators.append(language_model[chars])
-                else:
-                    numerators.append(1)
-                    current_total += 1
-                    miss_count += 1
-            probability = 0
-            for numerator in numerators:
-                probability += log10(numerator / float(current_total))
-            if miss_count < (len(char_list) - 3) * 0.75:
-                results.append((language, probability))
-        top_lang = ('other', 0)
-        if len(results) > 0:
-            top_lang = results[0]
-            for result in results[1:]:
-                if result[1] > top_lang[1]:
-                    top_lang = result
-        output_file.write(top_lang[0] + " " + line)
-    test_file.close()
-    output_file.close()
-'''
-
-# Code with defined model of smoothing
-def build_LM(in_file):
-    """
-    build language models for each label
-    each line in in_file contains a label and an URL separated by a tab(\t)
-    """
-    print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
-
     # the 4-gram language model
     smoothed_model = {}
-
     # the total number of occurrences for each language
     totals = {}
-
     # initialize the languages in the model
     input_file = file(in_file, 'r')
     for line in input_file:
@@ -109,20 +29,19 @@ def build_LM(in_file):
             smoothed_model[language] = {}
             totals[language] = 0
     input_file.close()
-
     # add data to language models
     input_file = file(in_file, 'r')
     for line in input_file:
         split_line = line.split(" ", 1)
         language = split_line[0]
-        char_list = list(split_line[1].strip())
-        for i in range(0, len(char_list) - 3):
-            chars = tuple(char_list[i:i+4])
+        # Convert sentence into a list of ngrams
+        ngrams = ngrams_for_string(NGRAM_SIZE, split_line[1].strip())
+        for ngram in ngrams:
             for lang in smoothed_model:
-                if chars not in smoothed_model[lang]:
-                    smoothed_model[lang][chars] = 1
+                if ngram not in smoothed_model[lang]:
+                    smoothed_model[lang][ngram] = 1
                     totals[lang] += 1
-            smoothed_model[language][chars] += 1
+            smoothed_model[language][ngram] += 1
             totals[language] += 1
     input_file.close()
     return (smoothed_model, totals)
@@ -141,308 +60,92 @@ def test_LM(in_file, out_file, LM):
 
     (smoothed_model, totals) = LM
 
-    line_count = 1
     for line in test_file:
-        char_list = list(line.strip())
+        # Convert sentence into a list of ngrams
+        ngrams = ngrams_for_string(NGRAM_SIZE, line.strip())
+        # The results of the probability calculation for each language
         results = []
         for language in smoothed_model:
             language_model = smoothed_model[language]
+            # The counts of each of the ngrams to be used as numerators in
+            # the probability calculation
             numerators = []
+            # The number of times a ngram is not found in the language model
             miss_count = 0
             lang_total = float(totals[language])
-            for i in range(0, len(char_list) - 3):
-                chars = tuple(char_list[i:i+4])
-                if chars in language_model:
-                    numerators.append(language_model[chars])
+            for ngram in ngrams:
+                if ngram in language_model:
+                    numerators.append(language_model[ngram])
                 else:
                     miss_count += 1
-            if len(numerators) > 0:
-                probability = reduce(lambda x,y: x + log10(y / lang_total), numerators)
-                if miss_count < (len(char_list) - 3) * 0.75:
-                    results.append((language, probability))
-        top_lang = ('other', 0)
-        line_count += 1
+            # Ignore result if the number of times ngrams of the sentence
+            # exceeds 75% of the total number of ngrams being tested for
+            # that sentence
+            if len(numerators) > 0 and miss_count < len(ngrams) * 0.75:
+                probability = 0
+                for num in numerators:
+                    probability += log10(num / lang_total)
+                results.append((language, probability))
+        top_lang = 'other'
         if len(results) > 0:
-            top_lang = results[0]
-            for result in results[1:]:
-                if result[1] > top_lang[1]:
-                    top_lang = result
-        output_file.write(top_lang[0] + " " + line)
+            top_lang = max(results, key = lambda x: x[1])[0]
+        output_file.write(top_lang + " " + line)
     test_file.close()
     output_file.close()
 
-# Unigram test
-'''
-# Code with defined model of smoothing
-def build_LM(in_file):
+def ngrams_for_string(n, string):
     """
-    build language models for each label
-    each line in in_file contains a label and an URL separated by a tab(\t)
+    Converts a string into a list of ngrams based on the size provided
+    Allows for customization of ngrams for essay questions
     """
-    print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
+    res = []
+    char_list = list(string)
+    for i in range(0, len(string) - n + 1):
+        chars = tuple(char_list[i : i + n])
+        res.append(chars)
+    return res
 
-    # the 4-gram language model
-    smoothed_model = {}
+########################
+# FOR ESSAY QUESTION 3 #
+########################
+# Test for strings stripped of numbers and punctuations
+# Also tests for strings converted to lowercase
+# Uncomment the function below to proceed with the test
+# def ngrams_for_string(n, sentence):
+#     res = []
+#     # Converts all characters to lowercase
+#     # sentence = sentence.lower()
+#    
+#     char_list = list(sentence)
+#   
+#     # Both punctuation and numbers removed
+#     char_list = filter(lambda x: x.isalpha() or x.isspace(), char_list)
+#   
+#     # Only punctuation removed
+#     # import string
+#     # char_list = filter(lambda x: x not in string.punctuation, char_list)
+#   
+#     # Only numeric removed
+#     # char_list = filter(lambda x: not x.isdigit(), char_list)
+#     for i in range(0, len(char_list) - n + 1):
+#         chars = tuple(char_list[i : i + n])
+#         res.append(chars)
+#     return res
+########################
+# END ESSAY QUESTION 3 #
+########################
 
-    # the total number of occurrences for each language
-    totals = {}
-
-    # initialize the languages in the model
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        if language not in smoothed_model:
-            smoothed_model[language] = {}
-            totals[language] = 0
-    input_file.close()
-
-    # add data to language models
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        char_list = list(split_line[1].strip())
-        for i in range(0, len(char_list)):
-            chars = tuple(char_list[i:i+1])
-            for lang in smoothed_model:
-                if chars not in smoothed_model[lang]:
-                    smoothed_model[lang][chars] = 1
-                    totals[lang] += 1
-            smoothed_model[language][chars] += 1
-            totals[language] += 1
-    input_file.close()
-    return (smoothed_model, totals)
-
-def test_LM(in_file, out_file, LM):
-    """
-    test the language models on new URLs
-    each line of in_file contains an URL
-    you should print the most probable label for each URL into out_file
-    """
-    print "testing language models..."
-    # This is an empty method
-    # Pls implement your code in below
-    test_file = file(in_file, 'r')
-    output_file = file(out_file, 'w')
-
-    smoothed_model = LM[0]
-    totals = LM[1]
-
-    for line in test_file:
-        char_list = list(line.strip())
-        results = []
-        for language in smoothed_model:
-            language_model = smoothed_model[language]
-            numerators = []
-            miss_count = 0
-            for i in range(0, len(char_list)):
-                chars = tuple(char_list[i:i+1])
-                if chars in language_model:
-                    numerators.append(language_model[chars])
-                else:
-                    miss_count += 1
-            new_total = float(totals[language] + miss_count)
-            if len(numerators) > 0:
-                probability = reduce(lambda x,y: x + log10(y / new_total), numerators)
-                if miss_count > 0:
-                    probability += reduce(lambda x,y: x + log10(1.0 / new_total), range(0, miss_count))
-                if miss_count < (len(char_list) - 3) * 0.75:
-                    results.append((language, probability))
-        top_lang = ('other', 0)
-        if len(results) > 0:
-            top_lang = results[0]
-            for result in results[1:]:
-                if result[1] > top_lang[1]:
-                    top_lang = result
-        output_file.write(top_lang[0] + " " + line)
-    test_file.close()
-    output_file.close()
-'''
-
-# Bigram model
-'''
-# Code with defined model of smoothing
-def build_LM(in_file):
-    """
-    build language models for each label
-    each line in in_file contains a label and an URL separated by a tab(\t)
-    """
-    print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
-
-    # the 4-gram language model
-    smoothed_model = {}
-
-    # the total number of occurrences for each language
-    totals = {}
-
-    # initialize the languages in the model
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        if language not in smoothed_model:
-            smoothed_model[language] = {}
-            totals[language] = 0
-    input_file.close()
-
-    # add data to language models
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        char_list = list(split_line[1].strip())
-        for i in range(0, len(char_list)-1):
-            chars = tuple(char_list[i:i+2])
-            for lang in smoothed_model:
-                if chars not in smoothed_model[lang]:
-                    smoothed_model[lang][chars] = 1
-                    totals[lang] += 1
-            smoothed_model[language][chars] += 1
-            totals[language] += 1
-    input_file.close()
-    return (smoothed_model, totals)
-
-def test_LM(in_file, out_file, LM):
-    """
-    test the language models on new URLs
-    each line of in_file contains an URL
-    you should print the most probable label for each URL into out_file
-    """
-    print "testing language models..."
-    # This is an empty method
-    # Pls implement your code in below
-    test_file = file(in_file, 'r')
-    output_file = file(out_file, 'w')
-
-    smoothed_model = LM[0]
-    totals = LM[1]
-
-    for line in test_file:
-        char_list = list(line.strip())
-        results = []
-        for language in smoothed_model:
-            language_model = smoothed_model[language]
-            numerators = []
-            miss_count = 0
-            for i in range(0, len(char_list)-1):
-                chars = tuple(char_list[i:i+2])
-                if chars in language_model:
-                    numerators.append(language_model[chars])
-                else:
-                    miss_count += 1
-            new_total = float(totals[language] + miss_count)
-            if len(numerators) > 0:
-                probability = reduce(lambda x,y: x + log10(y / new_total), numerators)
-                if miss_count > 0:
-                    probability += reduce(lambda x,y: x + log10(1.0 / new_total), range(0, miss_count))
-                if miss_count < (len(char_list) - 3) * 0.75:
-                    results.append((language, probability))
-        top_lang = ('other', 0)
-        if len(results) > 0:
-            top_lang = results[0]
-            for result in results[1:]:
-                if result[1] > top_lang[1]:
-                    top_lang = result
-        output_file.write(top_lang[0] + " " + line)
-    test_file.close()
-    output_file.close()
-'''
-
-# Trigram model
-'''
-# Code with defined model of smoothing
-def build_LM(in_file):
-    """
-    build language models for each label
-    each line in in_file contains a label and an URL separated by a tab(\t)
-    """
-    print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
-
-    # the 4-gram language model
-    smoothed_model = {}
-
-    # the total number of occurrences for each language
-    totals = {}
-
-    # initialize the languages in the model
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        if language not in smoothed_model:
-            smoothed_model[language] = {}
-            totals[language] = 0
-    input_file.close()
-
-    # add data to language models
-    input_file = file(in_file, 'r')
-    for line in input_file:
-        split_line = line.split(" ", 1)
-        language = split_line[0]
-        char_list = list(split_line[1].strip())
-        for i in range(0, len(char_list)-2):
-            chars = tuple(char_list[i:i+3])
-            for lang in smoothed_model:
-                if chars not in smoothed_model[lang]:
-                    smoothed_model[lang][chars] = 1
-                    totals[lang] += 1
-            smoothed_model[language][chars] += 1
-            totals[language] += 1
-    input_file.close()
-    return (smoothed_model, totals)
-
-def test_LM(in_file, out_file, LM):
-    """
-    test the language models on new URLs
-    each line of in_file contains an URL
-    you should print the most probable label for each URL into out_file
-    """
-    print "testing language models..."
-    # This is an empty method
-    # Pls implement your code in below
-    test_file = file(in_file, 'r')
-    output_file = file(out_file, 'w')
-
-    smoothed_model = LM[0]
-    totals = LM[1]
-
-    for line in test_file:
-        char_list = list(line.strip())
-        results = []
-        for language in smoothed_model:
-            language_model = smoothed_model[language]
-            numerators = []
-            miss_count = 0
-            for i in range(0, len(char_list)-2):
-                chars = tuple(char_list[i:i+3])
-                if chars in language_model:
-                    numerators.append(language_model[chars])
-                else:
-                    miss_count += 1
-            new_total = float(totals[language] + miss_count)
-            if len(numerators) > 0:
-                probability = reduce(lambda x,y: x + log10(y / new_total), numerators)
-                if miss_count > 0:
-                    probability += reduce(lambda x,y: x + log10(1.0 / new_total), range(0, miss_count))
-                if miss_count < (len(char_list) - 3) * 0.75:
-                    results.append((language, probability))
-        top_lang = ('other', 0)
-        if len(results) > 0:
-            top_lang = results[0]
-            for result in results[1:]:
-                if result[1] > top_lang[1]:
-                    top_lang = result
-        output_file.write(top_lang[0] + " " + line)
-    test_file.close()
-    output_file.close()
-'''
+########################
+# FOR ESSAY QUESTION 4 #
+########################
+# Tests for other n-gram sizes
+# Uncomment the size of the ngram you would like to test
+# NGRAM_SIZE = 1
+# NGRAM_SIZE = 2
+# NGRAM_SIZE = 3
+########################
+# END ESSAY QUESTION 4 #
+########################
 
 def usage():
     print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
