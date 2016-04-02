71d64aad93196a93515166f0bac3cccab6554d0e
ianngiaw <ianngiawtingan@gmail.com>
2016-04-02 16:17:38 +0800

    Added comments explaining next() return types

--- HW4/test.py
+++ HW4/test.py
@@ -5,8 +5,13 @@ import search
 
 postings = file("postings.txt")
 
+# next() return format: (doc_id, raw_term_frequency, normalized_log_weighted_term_frequency)
 reader1 = search.get_term_postings_reader("determin", term_dict, postings)
+
+# next() return format: (term, raw_term_frequency)
 reader2 = search.get_docs_postings_reader("US20090112905A1", docs_dict, postings)
+
+# next() return format: (doc_id)
 reader3 = search.get_doc_fields_postings_reader("US20090289046A1", doc_fields_dict, postings)
 
 readers = [reader1, reader2, reader3]
